895f4b4f1c25a53c07c101a50b855c1c1fc9a46b
Sanjay Menakuru <balasanjay@gmail.com>
2018-08-24 21:43:34 -0700

    Add a Dockerfile and build script for wile.
    This is not an ideal build setup; it'd be nice if the build itself could
    be done inside a container so its reproducible. But for now, we just
    build on the host, and copy the resulting binary into a Docker image.

---
+++ build_fproxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -o xtrace
+
+function main {
+  export WORKDIR=`mktemp -d`
+  trap "rm -rf $WORKDIR" EXIT
+
+  export CGO_ENABLED=0
+  export GO111MODULE=on
+
+  go build -tags netgo -o "$WORKDIR"/fproxy .
+
+  docker build -f ./Dockerfile -t fproxy:latest "$WORKDIR"
+}
+
+main "$@"
